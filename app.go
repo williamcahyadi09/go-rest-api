cef0e8c0ddcad7faf2b6b6dfd9ec6831f0ea1810
w.cahyadi <w.cahyadi@verihubs.com>
2023-04-23 21:23:30 +0700

    initial commit

---
+++ app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	url := "http://127.0.0.1:8000"
+	fmt.Println("Listening on", url)
+
+	http.ListenAndServe(":8000", r)
+}
